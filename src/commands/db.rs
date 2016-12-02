d84d2cf29f70eb506bbd3ce4574a8d05726de349
rushmorem <rushmore@webenchanter.com>
2016-12-02 17:53:44 +0200

    Document db and table commands

--- src/commands/db.rs
+++ src/commands/db.rs
@@ -1,18 +1,41 @@
+//! ReQL command: db
+//!
+//! ## Command syntax
+//!
+//! > r.db(dbName) → db
+//!
+//! ## Description
+//! 
+//! Reference a database.
+//!
+//! The `db` command is optional. If it is not present in a query, the query will run against the
+//! default database for the connection, specified in the `db` argument to [connect](../connect/index.html).
+//!
+//! ## Example
+//!
+//! Explicitly specify a database for a query.
+//!
+//! ```norun
+//! r.db("heroes").table("marvel").run();
+//! ```
+//!
+//! ## Related commands
+//!
+//! * [table](../table/index.html)
+//! * [db_list](../db_list/index.html)
+
+#![allow(dead_code)]
+
 use types;
 use types::Command as Cmd;
 use proto::Term_TermType as TermType;
-use super::{Command, Client};
+use super::Command;
 
-#[allow(dead_code)]
-impl Client {
+impl Command<(), ()> {
+    /// Reference a database. [Read more](db/index.html)
     pub fn db<T>(&self, arg: T) -> Command<types::Db, ()> where
         T: Into<types::String>
         {
-            Cmd::make(
-                TermType::DB,
-                Some(arg.into()),
-                None,
-                Root!(),
-                )
+            Cmd::make(TermType::DB, Some(arg.into()), None, Root!())
         }
 }
