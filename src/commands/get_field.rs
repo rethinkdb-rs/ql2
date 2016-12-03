88ff082990ce8864b69e59b96c2ad04d76eedff0
rushmorem <rushmore@webenchanter.com>
2016-12-03 21:55:05 +0200

    Initial map implementation

--- src/commands/get_field.rs
+++ src/commands/get_field.rs
@@ -1,10 +1,11 @@
+#![allow(dead_code)]
+
 use types;
 use types::Command as Cmd;
 use proto::Term_TermType as TermType;
 use super::Command;
 use serde_json::value::ToJson;
 
-#[allow(dead_code)]
 impl<O> Command<types::Object, O>
 where O: ToJson + Clone
 {
@@ -15,7 +16,6 @@ where O: ToJson + Clone
         }
 }
 
-#[allow(dead_code)]
 impl<O> Command<types::Array, O>
 where O: ToJson + Clone
 {
