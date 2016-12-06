738590e2a7f890a568c3285e2cfcc4324b8ea6d1
rushmorem <rushmore@webenchanter.com>
2016-12-06 09:12:36 +0200

    Split out closure

--- src/commands/map.rs
+++ src/commands/map.rs
@@ -62,17 +62,31 @@ pub trait Stream: types::DataType {}
 
 impl Stream for types::Table {}
 
-impl<F, CT, CO, T, O> From<(Command<CT, CO>, F)> for MapArg<types::Stream>
+impl<F, T, O> From<F> for types::Function
+    where T: types::DataType,
+          O: ToJson + Clone,
+          F: Fn(Arg, Arg) -> Command<T, O>,
+{
+    fn from(t: F) -> types::Function {
+        let res = t(var!(), var!());
+        let term = func!(res.into());
+        From::from(term)
+    }
+}
+
+impl<F, CT, CO> From<(Vec<Command<CT, CO>>, F)> for MapArg<types::Stream>
     where CT: Stream,
           CO: ToJson + Clone,
-          T: types::DataType,
-          O: ToJson + Clone,
-          F: Fn(Arg, Arg) -> Command<T, O>
+          F: Into<types::Function>
 {
-    fn from(t: (Command<CT, CO>, F)) -> MapArg<types::Stream> {
-        let arg: Term = t.0.into();
-        let res = t.1(var!(), var!());
-        let term = func!(res.into());
-        MapArg(vec![arg.into(), term.into()])
+    fn from(t: (Vec<Command<CT, CO>>, F)) -> MapArg<types::Stream> {
+        let mut args = Vec::with_capacity(t.0.len()+1);
+        for arg in t.0 {
+            let arg: Term = arg.into();
+            args.push(arg.into());
+        }
+        let func: Term = t.1.into().into();
+        args.push(func.into());
+        MapArg(args)
     }
 }
