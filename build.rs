1afebe637407b5d94c0d89e7932d3229392fe87d
rushmorem <rushmore@webenchanter.com>
2021-04-07 16:01:04 +0200

    add serde support

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> std::io::Result<()> {
-    prost_build::compile_protos(&["src/ql2.proto"], &["src/"])?;
+    prost_build::Config::new()
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile_protos(&["src/ql2.proto"], &["src/"])?;
     Ok(())
 }
